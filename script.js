6f5574a9fde21ff55d3d7755d8ad69a97511bb28
unknown <mirla.duartep@gmail.com>
2022-09-29 17:57:55 -0300

    correções no layout

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const question = document.querySelectorAll('.question');
 const answer = document.querySelectorAll('.answer');
 const section = document.querySelectorAll('section');
-const myElement = document.querySelector('content');
+const content = document.querySelector('.content');
 
 function callback(event) {
     const target = event.target;
     const parent = target.parentNode;
-    const classAnswer = parent.querySelector('.answer');
+    const pai = parent.parentNode;
+    const classAnswer = pai.querySelector('.answer');
+    const image = pai.querySelector('img');
+    image.classList.toggle('active');
     classAnswer.classList.toggle('active');
     target.classList.toggle('active');
 }
